3bd801b4dcd7aa38ea218f265904dd026861357d
Joel Gallun <joel.gallun@gmail.com>
2015-03-24 22:29:42 -0400

    add script to setup environment

---
+++ env.sh
@@ -0,0 +1,4 @@
+PATH=$PATH:/opt/brcm/hndtools-mipsel-linux/bin:/opt/brcm/hndtools-mipsel-uclibc/bin/
+export SRC=$HOME/asuswrt-merlin/release/src-rt-6.x
+export TGT=$SRC/router/mipsel-uclibc/target
+export IMG=$SRC/image
